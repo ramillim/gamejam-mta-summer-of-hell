54840f6dc7c8cefec03fc4cbe345d0ebae220352
ramillim <ramil.lim@gmail.com>
2017-08-12 03:31:54 -0400

    Set up a GameManager with scene transitions.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum GameState
+{
+    Intro,
+    Playing,
+    Paused,
+    GameOver
+}
+
+public delegate void OnStateChangeHandler();
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public event OnStateChangeHandler OnStateChange;
+
+    [SerializeField]
+    private GameState currentState;
+
+    public GameState CurrentState { get; private set; }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void SetState(GameState state)
+    {
+        CurrentState = state;
+        OnStateChange();
+    }
+}
---
+++ Assets/Scripts/GameStates/Intro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    GameManager gm;
+
+    private void Awake()
+    {
+        gm = GameManager.instance;
+        gm.OnStateChange += HandleOnStateChange;
+    }
+
+    private void HandleOnStateChange()
+    {
+        gm.SetState(GameState.Playing);
+        Invoke("LoadNextScene", 3f);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadSceneAsync("Sandbox");
+    }
+}
