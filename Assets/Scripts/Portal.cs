f6f1c5a96769a11ac77148b6dff16ab604d03778
Ramil Lim <ramil.lim@gmail.com>
2017-08-12 12:48:05 -0400

    Create a door portal in the sandbox that triggers on Use.

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+
+public class Portal : VRTK_InteractableObject
+{
+    public string nextScene;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+        // TODO: Animate Door
+        LoadScene();
+    }
+
+    private void LoadScene()
+    {
+        // TODO: Serialize and pass relevant data to the new scene.
+        SceneManager.LoadSceneAsync(nextScene);
+    }
+}
