242f2ca59c9f0f396931023e5a9ab05599c4b21a
Ramil Lim <ramil.lim@gmail.com>
2017-08-13 14:09:39 -0400

    Add audio. Add final scene with video.

---
+++ Assets/Scripts/GameStates/VideoScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoScene : MonoBehaviour {
+    public string nextScene;
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            LoadNextScene();
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadSceneAsync(nextScene);
+    }
+}
