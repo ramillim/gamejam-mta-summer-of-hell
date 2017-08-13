120dbe418176da48fcd73ac62f3cfce8532a8d88
ramillim <ramil.lim@gmail.com>
2017-08-13 07:41:01 -0400

    Fix collider for zombie. Add mouse to scene.

--- Assets/Scripts/Enemies/TakeDamage.cs
+++ Assets/Scripts/Enemies/TakeDamage.cs
@@ -6,12 +6,20 @@ public class TakeDamage : MonoBehaviour
 {
     public AudioClip hitSound;
     public AudioSource audioSource;
+    public float forceMultiplier = 1000f;
+    private Rigidbody rb;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Weapon")
         {
             audioSource.PlayOneShot(hitSound);
+            Vector3 collisionVector = (transform.position - collision.transform.position).normalized;
+            rb.AddForce(collisionVector * forceMultiplier);
             Destroy(gameObject, 1f);
         }
     }
