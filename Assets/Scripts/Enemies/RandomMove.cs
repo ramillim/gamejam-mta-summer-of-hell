ce9d9acc4f84b5d8fb3e80263f5c44575cc6c864
Ramil Lim <ramil.lim@gmail.com>
2017-08-13 10:57:11 -0400

    Add collider and take damage script to mouse. Bake lightning in subway scene 2.

---
+++ Assets/Scripts/Enemies/RandomMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RandomMove : MonoBehaviour
+{
+    public float moveRadius = 1f;
+
+    private NavMeshAgent agent;
+    private float elapsedTime = 0f;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (elapsedTime > 2f)
+        {
+            agent.destination = RandomNavSphere(transform.position, moveRadius, -1);
+            elapsedTime = 0f;
+        }
+        else
+        {
+            elapsedTime += Time.deltaTime;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float distance, int layermask)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * distance;
+
+        randomDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+
+        return navHit.position;
+    }
+}
