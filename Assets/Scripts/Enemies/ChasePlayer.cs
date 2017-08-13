0ec9352a28932af240400ac89756b6a07e150e5b
Ramil Lim <ramil.lim@gmail.com>
2017-08-13 10:36:49 -0400

    Regenerate lighting for scene 2. Create a chase player script.

---
+++ Assets/Scripts/Enemies/ChasePlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    private Transform goal;
+    private NavMeshAgent agent;
+    private float elapsedTime;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        goal = GameObject.FindWithTag("Player").transform;
+        AdjustCourse();
+    }
+
+    private void Update()
+    {
+        if (elapsedTime > 1f)
+        {
+            AdjustCourse();
+            elapsedTime = 0f;
+            return;
+        }
+
+        elapsedTime += Time.deltaTime;
+    }
+
+    private void AdjustCourse()
+    {
+        Vector3 newDestination = goal.position;
+        newDestination.y = transform.position.y;
+        agent.destination = newDestination;
+    }
+}
